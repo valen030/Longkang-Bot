c9bae0f0d7a5ce4453af2b5d56170f09e40a4ad8
valen ang <valenang030@gmail.com>
2024-03-16 13:52:09 +0800

    Add project files.

---
+++ WinFormsApp1/Program.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Threading;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Reflection;
+using System.Text;
+using System.Collections.Generic;
+using Discord.Audio;
+
+namespace WinFormsApp1
+{
+    internal static class Program
+    {
+        private static DiscordSocketClient? _client;
+#pragma warning disable CS0649 // Field 'Program.Context' is never assigned to, and will always have its default value null
+        private static SocketCommandContext? Context;
+#pragma warning restore CS0649 // Field 'Program.Context' is never assigned to, and will always have its default value null
+        public static string mssg = "";
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            _ = MainAsync();
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        public static async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            var token = "MTAwMzkyODkzNzg3NDg2NjI1OA.GiIYRV.5Ft0Hx_4UnBgRQ2DPCmbtfdVN1skEN0Qoy9Wqk";
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        public class CommandHandler
+        {
+            private readonly DiscordSocketClient _client;
+            private readonly CommandService _commands;
+
+            // Retrieve client and CommandService instance via ctor
+            public CommandHandler(DiscordSocketClient client, CommandService commands)
+            {
+                _commands = commands;
+                _client = client;
+            }
+
+            public async Task InstallCommandsAsync()
+            {
+                // Hook the MessageReceived event into our command handler
+                _client.MessageReceived += HandleCommandAsync;
+
+                // Here we discover all of the command modules in the entry 
+                // assembly and load them. Starting from Discord.NET 2.0, a
+                // service provider is required to be passed into the
+                // module registration method to inject the 
+                // required dependencies.
+                //
+                // If you do not use Dependency Injection, pass null.
+                // See Dependency Injection guide for more information.
+                await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(),
+                                                services: null);
+            }
+
+            private async Task HandleCommandAsync(SocketMessage messageParam)
+            {
+                // Don't process the command if it was a system message
+                var message = messageParam as SocketUserMessage;
+                if (message == null) return;
+
+                // Create a number to track where the prefix ends and the command begins
+                int argPos = 0;
+
+                // Determine if the message is a command based on the prefix and make sure no bots trigger commands
+                if (!(message.HasCharPrefix('!', ref argPos) ||
+                    message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+                    message.Author.IsBot)
+                    return;
+
+                // Create a WebSocket-based command context based on the message
+                var context = new SocketCommandContext(_client, message);
+                Console.WriteLine("testingg");
+                // Execute the command with the command context we just
+                // created, along with the service provider for precondition checks.
+                await _commands.ExecuteAsync(
+                    context: context,
+                    argPos: argPos,
+                    services: null);
+            }
+        }
+        [Command("join", RunMode = RunMode.Async)]
+        public static async Task JoinChannel(IVoiceChannel? channel = null)
+        {
+            // Get the audio channel
+            channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+            if (channel == null) { await Context.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+
+            // For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+            var audioClient = await channel.ConnectAsync();
+
+        }
+    }
+}
